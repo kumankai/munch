df21292f21c9dfcb821747355be1bb4f55492eb6
Jacob Angga <jacobmondangga@gmail.com>
2024-12-17 16:49:24 -0600

    Fixed minor db issues

---
+++ munch-api/verify_db.py
@@ -0,0 +1,9 @@
+from app import create_app, db
+from sqlalchemy import inspect
+
+app = create_app()
+
+with app.app_context():
+    inspector = inspect(db.engine)
+    tables = inspector.get_table_names()
+    print("Tables:", tables)
