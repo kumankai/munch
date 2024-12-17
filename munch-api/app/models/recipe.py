178c972f4cba33c7eb4656211cc8036513d1655f
Jacob Angga <jacobmondangga@gmail.com>
2024-12-16 18:12:29 -0600

    Updated table names

--- munch-api/app/models/recipe.py
+++ munch-api/app/models/recipe.py
@@ -1,12 +1,14 @@
 from app import db
 
 class Recipe(db.Model):
+    __tablename__ = 'Recipes'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     instructions = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(64), nullable=False)
     image_url = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)
 
     def __repr__(self):
         return f'<Recipe {self.id}, {self.title}>'
--- munch-api/app/models/user.py
+++ munch-api/app/models/user.py
@@ -1,6 +1,8 @@
 from app import db
 
 class User(db.Model):
+    __tablename__ = 'Users'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
