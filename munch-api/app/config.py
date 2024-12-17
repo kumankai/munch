179bb999a1b0cc7b96ae18071631bc5e38878ca0
Jacob Angga <jacobmondangga@gmail.com>
2024-12-17 16:06:20 -0600

    Migrated DB to postgres container

--- munch-api/app/config.py
+++ munch-api/app/config.py
@@ -1,2 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mydb.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
