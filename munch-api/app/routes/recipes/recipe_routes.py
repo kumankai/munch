6c7ee90118e6c2c800dedf58600982e043288e8b
Jacob Angga <jacobmondangga@gmail.com>
2024-12-18 18:03:03 -0600

    Defined ingredient services and recipe deletion

--- munch-api/app/routes/recipes/recipe_routes.py
+++ munch-api/app/routes/recipes/recipe_routes.py
@@ -1,6 +1,7 @@
 from app.routes.recipes import recipes
 from flask import request, jsonify
 from app.services.recipe_service import RecipeService
+from app.services.ingredient_service import IngredientService
 
 @recipes.route('/recipes/all/<int:user_id>', methods=['GET'])
 def get_user_recipes(user_id):
@@ -15,7 +16,8 @@ def get_recipe(recipe_id):
 @recipes.route('/recipes/create', methods=['POST'])
 def create_recipe():
     recipe_data = request.get_json()
-    recipe = RecipeService.create_recipe(recipe_data)
+    recipe: dict = RecipeService.create_recipe(recipe_data['main'])
+    IngredientService.save_all_ingredients(recipe['id'], recipe_data['ingredients'])
     return jsonify({ 'recipe' : recipe })
 
 @recipes.route('/recipes/update/<int:recipe_id>', methods=['POST'])
@@ -23,3 +25,14 @@ def update_recipe(recipe_id):
     recipe_data = request.get_json()
     new_recipe = RecipeService.update_recipe(recipe_id, recipe_data)
     return jsonify({ 'recipe': new_recipe })
+
+@recipes.route('/recipes/delete/<int:recipe_id>', methods=['DELETE'])
+def delete_recipe(recipe_id):
+    result1 = IngredientService.delete_ingredients_by_recipe_id(recipe_id)
+    result2 = RecipeService.delete_recipe(recipe_id)
+    if not result1 or not result2:
+        message = 'Something went wrong.'
+    else:
+        message = 'Recipe deleted.'
+
+    return jsonify({ 'message': message }) 
--- munch-api/app/services/ingredient_service.py
+++ munch-api/app/services/ingredient_service.py
@@ -9,13 +9,55 @@ class IngredientService:
         return ingredients
     
     @staticmethod
-    def save_ingredient(ingredient: dict) -> Ingredient:
+    def save_ingredient(ingredient_data: dict) -> Ingredient:
         new_ingredient = Ingredient(
-            name = ingredient['name'],
-            quantity = ingredient['quantity'],
-            unit = ingredient['unit'],
-            recipe_id = ingredient['recipe_id'],
+            name = ingredient_data['name'],
+            quantity = ingredient_data['quantity'],
+            unit = ingredient_data['unit'],
+            recipe_id = ingredient_data['recipe_id'],
         )
         db.session.add(new_ingredient)
         db.session.commit()
         return new_ingredient.to_dict()
+    
+    @staticmethod
+    def save_all_ingredients(recipe_id:int, ingredients: List[dict]) -> bool:
+        try:
+            for ingredient in ingredients:
+                ingredient['recipe_id'] = recipe_id
+                IngredientService.save_ingredient(ingredient)
+        except:
+            return False
+        
+    @staticmethod
+    def update_ingredient(ingredient_id: int, ingredient_data: dict) -> Optional[dict]:
+        ingredient: Ingredient = Ingredient.query.get(ingredient_id)
+        if not ingredient: return None
+
+        ingredient.name = ingredient_data.get('name', ingredient.name)
+        ingredient.quantity = ingredient_data.get('quantity', ingredient.quantity)
+        ingredient.unit = ingredient_data.get('unit', ingredient.unit)
+
+        db.session.commit()
+        return ingredient.to_dict()
+
+    @staticmethod
+    def update_all_ingredients_by_recipe_id(ingredients: List[dict]) -> bool:
+        try:
+            for ingredient in ingredients:
+                IngredientService.update_ingredient(ingredient['id'], ingredient)
+        except:
+            return False
+        
+    @staticmethod
+    def delete_ingredients_by_recipe_id(recipe_id: int) -> bool:
+        try:
+            ingredients: List[Ingredient] = [ingredient for ingredient in Ingredient.query.filter_by(recipe_id=recipe_id).all()]
+            if not ingredients: return False
+
+            for ingredient in ingredients:
+                db.session.delete(ingredient)
+
+            return True
+        except:
+            return False
