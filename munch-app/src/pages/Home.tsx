9ef466a3fea2c9e81aeb154f978536b383304e11
Jacob Angga <jacobmondangga@gmail.com>
2024-12-30 18:00:47 -0600

    Test commit

--- munch-app/src/pages/Home.tsx
+++ munch-app/src/pages/Home.tsx
@@ -2,9 +2,11 @@ import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
     const navigate = useNavigate();
+    const mode = import.meta.env.MODE;
     
     return (
         <div>
+            <p>{mode}</p>
             <h1>Welcome to Munch</h1>
             <button 
                     className="search-button"
