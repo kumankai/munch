874b09cb21a85b397d55e8326e69ff26b0e237fc
Jacob Angga <jacobmondangga@gmail.com>
2024-12-30 18:24:31 -0600

    test

--- munch-app/src/api/config.ts
+++ munch-app/src/api/config.ts
@@ -2,4 +2,4 @@ const isDevelopment = import.meta.env.MODE === 'development';
 
 export const API_BASE_URL = isDevelopment 
   ? 'http://localhost:4000/api'
-  : import.meta.env.VITE_API_URL;
+  : import.meta.env.VITE_API_URL + '/api';
